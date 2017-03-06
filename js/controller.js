864d5ca2d5cf9f978e5a0bd3bfc98145341ce8b8
Philneeves <p.neeves@gmail.com>
2017-03-06 17:32:32 +0000

    additional audio files added

--- js/controller.js
+++ js/controller.js
@@ -23,7 +23,10 @@ angular.module('RouteControllers', [])
 	})
 	.controller('namesCtrl', function($scope) {
 	    $scope.names = [
-	        'What a surprise',
+	        'What a surprise' <audio controls>
+			<source src="../audio/AcousticGuitar1.mp3" type="audio/mpeg">
+			Your browser does not support the audio element.
+			</audio>,
 			'Break It Down',
 			'Hide Away',
 			'Saving My Love',
